84cb3a85aea2db6adb2758421195a7fa48bcab0f
raflord <rafaelwrobel2000@gmail.com>
2026-03-29 20:42:17 -0300

    updating configs

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -28,7 +28,9 @@ function Index() {
   return (
     <div className="max-w-xl mx-auto flex flex-col gap-6 items-center justify-center min-h-screen">
       <h1 className="text-5xl font-black">bevr</h1>
-      <h2 className="text-2xl font-bold">Bun + Elysia + Vite + React</h2>
+      <h2 className="text-2xl font-bold">
+        Bun + Elysia + Kysely + better-auth + React
+      </h2>
       <p>A typesafe fullstack monorepo</p>
       <div className="flex items-center gap-4">
         <Button onClick={() => sendRequest()}>Call API</Button>
